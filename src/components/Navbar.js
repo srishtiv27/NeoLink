8dd32b443b31347fa4e52fa02de5d8bf6883f695
RosaliaSL <rosaliaslwork@gmail.com>
2023-03-15 01:04:00 +0530

    Link logo to landing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import logo from '../images/neolink-logo.png';
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} className="nav--icon" alt="logo"/>
-            <h2 className="nav--logo_text">NeoLink</h2>
+            <a style={{textDecoration: 'none', color: 'white'}} href="/"><img src={logo} className="nav--icon" alt="logo" href="/"/></a>
+            <h2 className="nav--logo_text"><a style={{textDecoration: 'none', color: 'white'}} href="/">NeoLink</a></h2>
             <h3 className="nav--item"> <a style={{textDecoration: 'none', color: 'white'}} href="/neonatal-care">Neonatal Care</a></h3>
             <h3 className="nav--item"> <a style={{textDecoration: 'none', color: 'white'}} href="/volunteer">Become a volunteer</a></h3>
             <h3 className="nav--item">About Us</h3>
